c06e6c17326a3339bade2d04b88b36ea1c89bb39
shoraaa <shoraaa@protonmail.com>
2024-02-19 23:34:04 +0700

    update faco.cpp

--- src/faco.cpp
+++ src/faco.cpp
@@ -1895,7 +1895,7 @@ run_raco(const ProblemInstance &problem,
     auto ants_count = opt.ants_count_;
     const auto steps = opt.steps_;
     const auto actual_ants_count = ants_count * (1 << ((iterations + steps - 1) / steps));
-    cout << "Steps: " steps << ", actual ants count: " << actual_ants_count << endl;
+    cout << "Steps: " << steps << ", actual ants count: " << actual_ants_count << endl;
 
     vector<Ant> ants(actual_ants_count);
     for (auto &ant : ants) {
