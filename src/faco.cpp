6df5b21df0b893cdce27f0ffaa057979fb84fe74
shoraaa <shoraaa@protonmail.com>
2024-02-20 19:03:05 +0700

    update faco.cpp

--- src/faco.cpp
+++ src/faco.cpp
@@ -2501,7 +2501,7 @@ run_dynamic_raco(const ProblemInstance &problem,
     comp_log("local search time", ls_time);
     comp_log("loop count", loop_count);
 
-    return unique_ptr<Solution>(dynamic_cast<Solution*>(best_ant.release()));
+    return make_unique<Solution>(best_ant.route_, best_ant.cost_);
 }
 
 
